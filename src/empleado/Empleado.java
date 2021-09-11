2900613b60855f39fc8f41c0bdf1d07c65276725
juan <dpatriciaosorio@misena.edu.co>
2021-09-10 19:08:07 -0500

    se crea clase emplado

--- src/empleado/Empleado.java
+++ src/empleado/Empleado.java
@@ -10,5 +10,65 @@ package empleado;
  * @author 505
  */
 public class Empleado {
+
     
+    //atributos=variables
+    private int id;
+    private String nombre;
+    private String cargo;
+    private double horaSemanales;
+    private int salario;
+    
+    //Metodo=accion
+    //constructor
+    public Empleado() {
+              
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getHoraSemanales() {
+        return horaSemanales;
+    }
+
+    public void setHoraSemanales(double horaSemanales) {
+        this.horaSemanales = horaSemanales;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    
+    }
+   
+    //metodo
+    public int calcularSalario(double horas, int valor){
+    return 0;
 }
